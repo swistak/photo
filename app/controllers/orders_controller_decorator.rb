b71d8c1217d50a0280b0a81aaf7491a991bd7edd
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2010-01-12 23:18:32 +0100

    Second step of checkout

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -1,7 +1,20 @@
 OrdersController.class_eval do
   def approve
+    if request.post?
+
+      flash[:notice] = t('order_updated')
+    else
+      @checkout = Checkout.new(:order => @order)
+    end
+    
+  rescue Spree::GatewayError => ge
+    logger.debug("#{ge} \n #{ge.backtrace.join("\n")}")
+    flash[:error] = "#{ge.message}"
+  end
+
+  def reeject
     Order.transaction do
-      @order.approve!
+      @order.reeject_preview!
     end
     flash[:notice] = t('order_updated')
   rescue Spree::GatewayError => ge
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,4 @@ map.resources :orders do |order|
   order.resources :images, :path_prefix => '/orders/:order_id'
 end
 
-map.resources :orders, :member => {:approve => :post}
+map.resources :orders, :member => {:approve => :any, :reeject => :post}
